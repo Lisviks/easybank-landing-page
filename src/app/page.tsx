308bb1a1c1f36a970658a55da239ea0673419c2b
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2023-06-17 20:48:40 +0100

    Hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Header from './components/Header';
+import Hero from './components/Hero';
 
 export default function Home() {
   return (
     <>
       <Header />
       <main>
-        <h1>Easybank landing page</h1>
+        <Hero />
       </main>
       <footer></footer>
     </>

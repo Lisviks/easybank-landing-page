a826a1d54147f43537954a7b77567ef5e836feae
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2023-06-17 20:36:54 +0100

    Header component

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import Image from 'next/image';
+import Logo from '../../../public/images/logo.svg';
+import HamburgerIcon from '../../../public/images/icon-hamburger.svg';
+import CloseIcon from '../../../public/images/icon-close.svg';
+import { useState } from 'react';
+import Link from 'next/link';
+
+export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <header>
+      <div className='logo'>
+        <Image src={Logo} alt='Easybank' />
+      </div>
+      <div className='menu-toggle' onClick={() => setIsOpen(!isOpen)}>
+        <Image src={isOpen ? CloseIcon : HamburgerIcon} alt='menu toggle' />
+      </div>
+      <nav className={isOpen ? 'open' : ''}>
+        <li>
+          <Link href='#'>Home</Link>
+        </li>
+        <li>
+          <Link href='#'>About</Link>
+        </li>
+        <li>
+          <Link href='#'>Contact</Link>
+        </li>
+        <li>
+          <Link href='#'>Blog</Link>
+        </li>
+        <li>
+          <Link href='#'>Careers</Link>
+        </li>
+      </nav>
+    </header>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import Header from './components/Header';
+
 export default function Home() {
   return (
-    <main>
-      <h1>Easybank landing page</h1>
-    </main>
+    <>
+      <Header />
+      <main>
+        <h1>Easybank landing page</h1>
+      </main>
+      <footer></footer>
+    </>
   );
 }

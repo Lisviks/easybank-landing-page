cd57644a1a679e4cfa2b7dd28a521bd7514dce9c
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2023-07-11 17:02:39 +0100

    desktop section-1 scss

--- src/app/components/Section1.tsx
+++ src/app/components/Section1.tsx
@@ -7,50 +7,54 @@ import Image from 'next/image';
 export default function Section1() {
   return (
     <section className='section-1'>
-      <div>
-        <h2>Why choose Easybank?</h2>
-        <p>
-          We leverage Open Banking to turn your bank account into your financial hub. Control your finances like never
-          before.
-        </p>
-      </div>
-      <div>
-        <div className='image'>
-          <Image src={OnlineIcon} alt='icon' />
+      <div className='content'>
+        <div>
+          <h2>Why choose Easybank?</h2>
+          <p>
+            We leverage Open Banking to turn your bank account into your financial hub. Control your finances like never
+            before.
+          </p>
         </div>
-        <h3>Online Banking</h3>
-        <p>
-          Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.
-        </p>
-      </div>
-      <div>
-        <div className='image'>
-          <Image src={BudgetingIcon} alt='icon' />
+        <div>
+          <div className='image'>
+            <Image src={OnlineIcon} alt='icon' />
+          </div>
+          <h3>Online Banking</h3>
+          <p>
+            Our modern web and mobile applications allow you to keep track of your finances wherever you are in the
+            world.
+          </p>
         </div>
-        <h3>Simple Budgeting</h3>
-        <p>
-          See exactly where your money goes each month. Receive notifications when you’re close to your hitting limits.
-        </p>
-      </div>
-      <div>
-        <div className='image'>
-          <Image src={OnboardingIcon} alt='icon' />
+        <div>
+          <div className='image'>
+            <Image src={BudgetingIcon} alt='icon' />
+          </div>
+          <h3>Simple Budgeting</h3>
+          <p>
+            See exactly where your money goes each month. Receive notifications when you’re close to your hitting
+            limits.
+          </p>
         </div>
-        <h3>Fast Onboarding</h3>
-        <p>
-          We don’t do branches. Open your account in minutes online and start taking control of your finances right
-          away.
-        </p>
-      </div>
-      <div>
-        <div className='image'>
-          <Image src={ApiIcon} alt='icon' />
+        <div>
+          <div className='image'>
+            <Image src={OnboardingIcon} alt='icon' />
+          </div>
+          <h3>Fast Onboarding</h3>
+          <p>
+            We don’t do branches. Open your account in minutes online and start taking control of your finances right
+            away.
+          </p>
         </div>
-        <h3>Open API</h3>
-        <p>
-          Manage your savings, investments, pension, and much more from one account. Tracking your money has never been
-          easier.
-        </p>
+        <div>
+          <div className='image'>
+            <Image src={ApiIcon} alt='icon' />
+          </div>
+          <h3>Open API</h3>
+          <p>
+            Manage your savings, investments, pension, and much more from one account. Tracking your money has never
+            been easier.
+          </p>
+        </div>
       </div>
     </section>
   );

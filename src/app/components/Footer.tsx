9e50df91a569b64514a45ea7594c2fc07f97d21c
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2023-07-11 17:32:19 +0100

    desktop footer scss

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -9,63 +9,65 @@ import InstagramLogo from '../../../public/images/icon-instagram.svg';
 export default function Footer() {
   return (
     <footer>
-      <div className='socials'>
-        <div className='logo'>
-          <Image src={Logo} alt='Easybank logo' />
+      <div>
+        <div className='socials'>
+          <div className='logo'>
+            <Image src={Logo} alt='Easybank logo' />
+          </div>
+          <ul>
+            <li>
+              <a href='#'>
+                <Image src={FacebookLogo} alt='Facebook' />
+              </a>
+            </li>
+            <li>
+              <a href='#'>
+                <Image src={YoutubeLogo} alt='Youtube' />
+              </a>
+            </li>
+            <li>
+              <a href='#'>
+                <Image src={TwitterLogo} alt='Twitter' />
+              </a>
+            </li>
+            <li>
+              <a href='#'>
+                <Image src={PinterestLogo} alt='Pinterest' />
+              </a>
+            </li>
+            <li>
+              <a href='#'>
+                <Image src={InstagramLogo} alt='Instagram' />
+              </a>
+            </li>
+          </ul>
         </div>
-        <ul>
-          <li>
-            <a href='#'>
-              <Image src={FacebookLogo} alt='Facebook' />
-            </a>
-          </li>
-          <li>
-            <a href='#'>
-              <Image src={YoutubeLogo} alt='Youtube' />
-            </a>
-          </li>
-          <li>
-            <a href='#'>
-              <Image src={TwitterLogo} alt='Twitter' />
-            </a>
-          </li>
-          <li>
-            <a href='#'>
-              <Image src={PinterestLogo} alt='Pinterest' />
-            </a>
-          </li>
-          <li>
-            <a href='#'>
-              <Image src={InstagramLogo} alt='Instagram' />
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className='links'>
-        <ul>
-          <li>
-            <a href='#'>About Us</a>
-          </li>
-          <li>
-            <a href='#'>Contact</a>
-          </li>
-          <li>
-            <a href='#'>Blog</a>
-          </li>
-          <li>
-            <a href='#'>Careers</a>
-          </li>
-          <li>
-            <a href='#'>Support</a>
-          </li>
-          <li>
-            <a href='#'>Privacy Policy</a>
-          </li>
-        </ul>
-      </div>
-      <div className='invite'>
-        <button>Request Invite</button>
-        <p>© Easybank. All Rights Reserved</p>
+        <div className='links'>
+          <ul>
+            <li>
+              <a href='#'>About Us</a>
+            </li>
+            <li>
+              <a href='#'>Contact</a>
+            </li>
+            <li>
+              <a href='#'>Blog</a>
+            </li>
+            <li>
+              <a href='#'>Careers</a>
+            </li>
+            <li>
+              <a href='#'>Support</a>
+            </li>
+            <li>
+              <a href='#'>Privacy Policy</a>
+            </li>
+          </ul>
+        </div>
+        <div className='invite'>
+          <button>Request Invite</button>
+          <p>© Easybank. All Rights Reserved</p>
+        </div>
       </div>
     </footer>
   );
